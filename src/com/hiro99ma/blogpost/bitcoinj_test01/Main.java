4be2c598623059eb6e1a5359b461709970701aec
hirokuma <hiro9ma@gmail.com>
2018-06-02 19:10:42 +0900

    first

---
+++ src/com/hiro99ma/blogpost/bitcoinj_test01/Main.java
@@ -0,0 +1,40 @@
+package com.hiro99ma.blogpost.bitcoinj_test01;
+
+import org.bitcoinj.core.Address;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.params.TestNet3Params;
+
+import java.io.File;
+
+class BitcoinjTest {
+    private NetworkParameters params;
+    private WalletAppKit wak;
+
+    BitcoinjTest() {
+        params = TestNet3Params.get();
+        wak = new WalletAppKit(params, new File("./wallet"), "wak") {
+            @Override
+            protected void onSetupCompleted() {
+                if (wallet().getKeyChainGroupSize() < 1) {
+                    wallet().importKey(new ECKey());
+                }
+            }
+        };
+        wak.startAsync();
+    }
+
+    void exec() {
+        wak.awaitRunning();
+        Address addr = wak.wallet().currentReceiveAddress();
+        System.out.println("addr = " + addr.toString());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        BitcoinjTest bjt = new BitcoinjTest();
+        bjt.exec();
+    }
+}
